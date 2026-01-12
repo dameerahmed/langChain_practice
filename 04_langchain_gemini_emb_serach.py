da77aaa675c4e216fcfa6a8a5325d67a325a36d9
dameerahmed <dameerahmedghouri05@gmail.com>
2026-01-12 03:57:28 -0800

    check

--- 04_langchain_gemini_emb_serach.py
+++ 04_langchain_gemini_emb_serach.py
@@ -16,17 +16,20 @@ document = [
     "i live in lahore pakistan",
     "i am a student",
     "i am from pakistan",
+    "i live in nawabshah pakistan",
     "i am 22 years old",
     "i am currently studying computer science",
     "i am a student of computer science",
+    "my friend ishrat",
 ]
 
 doc_vectors = model.embed_documents(document)
 
-que_vector = model.embed_query("tum kidher rahta ho?")
+que_vector = model.embed_query("i am 22 years old")
 
 similarity_scores = cosine_similarity([que_vector], doc_vectors)
 index, score = sorted(list(enumerate(similarity_scores[0])), key=lambda x: x[1])[-1]
 
+print("similarity_scores:", similarity_scores)
 print("Most similar document:", document[index])
 print("Similarity score:", score)
